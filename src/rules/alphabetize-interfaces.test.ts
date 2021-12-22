d536c5e676ef02a26893f7b437e2c6b4c7458341
Brandon Scott <contact@brandonscott.me>
2021-12-21 22:03:04 -0500

    Cleanup alphabetize-interfaces tests

--- src/rules/alphabetize-interfaces.test.ts
+++ src/rules/alphabetize-interfaces.test.ts
@@ -1,70 +1,81 @@
 import { Project } from "ts-morph";
+import { expectSourceFilesToMatch } from "../test/matchers";
 import { alphabetizeInterfaces } from "./alphabetize-interfaces";
 
 describe("alphabetizeInterfaces", () => {
     it("should sort properties in interface when there are unsorted properties", () => {
         // Arrange
         const project = new Project({ useInMemoryFileSystem: true });
-        const _interface = `
-            interface Example {
-                zeta?: string;
-                beta: Record<string, string>;
-                alpha: number;
-                omicron: () => void;
-            }
-        `;
+        const input = project.createSourceFile(
+            "input.ts",
+            `
+                interface Example {
+                    zeta?: string;
+                    beta: Record<string, string>;
+                    alpha: number;
+                    omicron: () => void;
+                }
+            `
+        );
 
-        const expected = `
-            interface Example {
-                alpha: number;
-                beta: Record<string, string>;
-                omicron: () => void;
-                zeta?: string;
-            }
-        `;
-        const file = project.createSourceFile("interface.ts", _interface);
+        const expected = project.createSourceFile(
+            "expected.ts",
+            `
+                interface Example {
+                    alpha: number;
+                    beta: Record<string, string>;
+                    omicron: () => void;
+                    zeta?: string;
+                }
+            `
+        );
 
         // Act
-        const result = alphabetizeInterfaces(file);
+        const result = alphabetizeInterfaces(input);
 
         // Assert
-        expect(result.file.getFullText()).toStrictEqual(expected);
+        expectSourceFilesToMatch(result.file, expected);
     });
 
     it("should sort properties in all interfaces when there are multiple unsorted interfaces", () => {
         // Arrange
         const project = new Project({ useInMemoryFileSystem: true });
-        const _interface = `
-            interface Car {
-                make: string;
-                wheels: number;
-                model: string;
-            }
+        const input = project.createSourceFile(
+            "input.ts",
+            `
+                interface Car {
+                    make: string;
+                    wheels: number;
+                    model: string;
+                }
 
-            interface Dog {
-                name: string;
-                legs: number;
-            }
-        `;
+                interface Dog {
+                    name: string;
+                    legs: number;
+                }
+            `
+        );
 
-        const expected = `
-            interface Car {
-                make: string;
-                model: string;
-                wheels: number;
-            }
+        const expected = project.createSourceFile(
+            "expected.ts",
+            `
+                interface Car {
+                    make: string;
+                    model: string;
+                    wheels: number;
+                }
 
-            interface Dog {
-                legs: number;
-                name: string;
-            }
-        `;
-        const file = project.createSourceFile("interface.ts", _interface);
+                interface Dog {
+                    legs: number;
+                    name: string;
+                }
+            `
+        );
 
         // Act
-        const result = alphabetizeInterfaces(file);
+        const result = alphabetizeInterfaces(input);
 
         // Assert
-        expect(result.file.getFullText()).toStrictEqual(expected);
+        expectSourceFilesToMatch(result.file, expected);
     });
 });
