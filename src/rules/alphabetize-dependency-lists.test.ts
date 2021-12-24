4b79ee20350970ce1187111f2e8e67b08c605dd9
Brandon Scott <contact@brandonscott.me>
2021-12-24 12:11:56 -0500

    Simplify + add edge case tests

--- src/rules/alphabetize-dependency-lists.test.ts
+++ src/rules/alphabetize-dependency-lists.test.ts
@@ -3,31 +3,96 @@ import { expectSourceFilesToMatch } from "../test/matchers";
 import { alphabetizeDependencyLists } from "./alphabetize-dependency-lists";
 
 describe("alphabetizeDependencyLists", () => {
-    it.only("should alphabetize variables in dependency list", async () => {
-        // Arrange
-        const project = new Project({ useInMemoryFileSystem: true });
-        const input = project.createSourceFile(
-            "input.tsx",
-            `
-                useEffect(() => {
+    it.each(["useCallback", "useEffect", "useMemo"])(
+        "should alphabetize variables in %p dependency list",
+        async (functionName: string) => {
+            // Arrange
+            const project = new Project({ useInMemoryFileSystem: true });
+            const input = project.createSourceFile(
+                "input.tsx",
+                `
+                const value = ${functionName}(() => {
 
                 }, [setProject, handleOpenDialog, isLoading])
             `
-        );
+            );
 
-        const expected = project.createSourceFile(
-            "expected.tsx",
-            `
-                useEffect(() => {
+            const expected = project.createSourceFile(
+                "expected.tsx",
+                `
+                const value = ${functionName}(() => {
 
                 }, [handleOpenDialog, isLoading, setProject])
             `
-        );
+            );
 
-        // Act
-        const result = await alphabetizeDependencyLists(input);
+            // Act
+            const result = await alphabetizeDependencyLists(input);
 
-        // Assert
-        expectSourceFilesToMatch(result.file, expected);
-    });
+            // Assert
+            expectSourceFilesToMatch(result.file, expected);
+        }
+    );
+
+    it.each(["useCallback", "useEffect", "useMemo"])(
+        "should not change %p calls that don't have a second argument",
+        async (functionName: string) => {
+            // Arrange
+            const project = new Project({ useInMemoryFileSystem: true });
+            const input = project.createSourceFile(
+                "input.tsx",
+                `
+                const value = ${functionName}(() => {
+
+                })
+            `
+            );
+
+            const expected = project.createSourceFile(
+                "expected.tsx",
+                `
+                const value = ${functionName}(() => {
+
+                })
+            `
+            );
+
+            // Act
+            const result = await alphabetizeDependencyLists(input);
+
+            // Assert
+            expectSourceFilesToMatch(result.file, expected);
+        }
+    );
+
+    it.each(["useCallback", "useEffect", "useMemo"])(
+        "should not change %p calls that don't have an ArrayLiteralExpression as second argument",
+        async (functionName: string) => {
+            // Arrange
+            const project = new Project({ useInMemoryFileSystem: true });
+            const input = project.createSourceFile(
+                "input.tsx",
+                `
+                    const value = ${functionName}(() => {
+
+                    }, "something")
+                `
+            );
+
+            const expected = project.createSourceFile(
+                "expected.tsx",
+                `
+                    const value = ${functionName}(() => {
+
+                    }, "something")
+                `
+            );
+
+            // Act
+            const result = await alphabetizeDependencyLists(input);
+
+            // Assert
+            expectSourceFilesToMatch(result.file, expected);
+        }
+    );
 });
