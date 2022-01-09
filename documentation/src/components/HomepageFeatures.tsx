43b5fb8c6dec660659b6cba95347b8571483a166
Brandon Scott <contact@brandonscott.me>
2022-01-09 14:20:41 -0500

    Update HomepageFeatures copy

--- documentation/src/components/HomepageFeatures.tsx
+++ documentation/src/components/HomepageFeatures.tsx
@@ -1,70 +1,70 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 type FeatureItem = {
-  title: string;
-  image: string;
-  description: JSX.Element;
+    title: string;
+    image: string;
+    description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    image: '/img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    image: '/img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    image: '/img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: "Code should be easy to read",
+        image: "/img/undraw_docusaurus_mountain.svg",
+        description: (
+            <>
+                The more consistent your codebase is, the easier it is to
+                maintain
+            </>
+        ),
+    },
+    {
+        title: "Consistency should be automated",
+        image: "/img/undraw_docusaurus_tree.svg",
+        description: (
+            <>
+                Code consistency should be enforced by tooling to avoid
+                unnecessary mental cycles and effort
+            </>
+        ),
+    },
+    {
+        title: "Plug & Play",
+        image: "/img/undraw_docusaurus_react.svg",
+        description: (
+            <>
+                Pick and choose the rules that you want to use, run on specific
+                files only, or use it as a linter to fail CI builds
+            </>
+        ),
+    },
 ];
 
-function Feature({title, image, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img className={styles.featureSvg} alt={title} src={image} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, image, description }: FeatureItem) {
+    return (
+        <div className={clsx("col col--4")}>
+            <div className="text--center">
+                <img className={styles.featureSvg} alt={title} src={image} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
