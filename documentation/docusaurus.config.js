fed4c91f9a522623ead6270a6b120796912dc7ef
Brandon Scott <contact@brandonscott.me>
2022-01-28 09:41:53 -0500

    Fix incorrect link in docusaurus config, add CNAME record

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
                         items: [
                             {
                                 label: "GitHub",
-                                href: "https://github.com/facebook/docusaurus",
+                                href: "https://github.com/brandongregoryscott/collation",
                             },
                             {
                                 label: "npm",
