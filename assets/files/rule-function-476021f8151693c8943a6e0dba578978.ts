b7123a1e39b180bab46238cf7e4dcb28765e8771
Brandon Scott <contact@brandonscott.me>
2022-06-08 20:18:36 -0400

    Deploy website - based on cd84096b5f6a743f37d382da7593efa4c00b4d27

--- assets/files/rule-function-476021f8151693c8943a6e0dba578978.ts
+++
@@ -1,6 +0,0 @@
-import { SourceFile } from "ts-morph";
-import { RuleResult } from "../interfaces/rule-result";
-
-type RuleFunction = (file: SourceFile) => Promise<RuleResult>;
-
-export { RuleFunction };
